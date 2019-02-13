404db07ca61754d5814338e33216eb6bd02d2457
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2019-02-14 01:08:06 +0530

    Update demo.js

--- Read from files/demo.js
+++ Read from files/demo.js
@@ -1,9 +1,15 @@
+// importing filesystem module
 var fs = require('fs')
+// importing data.json file path
 var data = require('./data.json')
 
+// 
 console.log(data.name)
 
+// reading the file
 fs.readFile('./data.json', 'utf-8', (err,data) => {
+    // reading the file and parsing the json content inside the file
     var data = JSON.parse(data)
+    // printing 
     console.log(data.name)
 })
