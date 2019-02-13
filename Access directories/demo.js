e7fdaf48d4142001c056130fd47b26f9b38ed507
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2019-02-14 00:53:30 +0530

    Update demo.js

--- Access directories/demo.js
+++ Access directories/demo.js
@@ -1,5 +1,8 @@
+// importing the filesystem module
 var fs = require('fs')
 
+// reading all the directories from c Drive
 fs.readdir('c:/', (err, data) => {
+    // displaying the output over the console
     console.log(data)
 })
