0d328b0c979db03e0d660ae269d4af8e85ffbdb4
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2019-02-14 00:58:37 +0530

    Update demo.js

--- Write to a file/demo.js
+++ Write to a file/demo.js
@@ -1,9 +1,13 @@
+// importing the filesystem module
 var fs = require('fs')
 
+// creating a json object
 var data = {
     name: 'Bob'
 }
 
+// writing inside the file named data.json with the json object we just created
 fs.writeFile('data.json', JSON.stringify(data), (err) =>{
+    // once we are done with writing the files 'write finished' message is pop up on the console
     console.log('write finished', err)
 })
