067234eab90f4a14baeb95452b11c3aefd48b957
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2019-02-17 14:47:43 +0530

    comments added

--- Static serving with Express/server.js
+++ Static serving with Express/server.js
@@ -1,7 +1,12 @@
+//  importing express
 var express = require('express')
+// importing app from express
 var app = express()
 
+// giving the path to serve static assets
 app.use(express.static(__dirname))
+
+// serving the sever on port 3000 
 var server = app.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
 })
